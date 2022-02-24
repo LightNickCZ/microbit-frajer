7152b8a7471e2b90201fb129187acae8eac107a9
Nick <74722819+LightNickCZ@users.noreply.github.com>
2022-02-24 15:01:22 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(7)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.SouthWest)
+})
 basic.showNumber(69)
 basic.showString("hh")
-basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        . . . . .
-        `)
-    basic.pause(2000)
-    basic.clearScreen()
-    basic.pause(2000)
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        . # # # .
-        # . . . #
-        . . . . .
-        `)
-    basic.pause(2000)
-    basic.clearScreen()
-    basic.pause(2000)
-    basic.showIcon(IconNames.EigthNote)
-})
