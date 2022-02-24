5e90a280c9e001f290c0d2e39d58a49fd678dc70
Nick <74722819+LightNickCZ@users.noreply.github.com>
2022-02-24 15:13:17 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(7)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        . . . . .
-        `)
+    led.plot(1, 0)
+    basic.pause(1000)
+    led.unplot(1, 0)
+    led.plot(1, 1)
+    basic.pause(1000)
+    led.unplot(1, 1)
+    led.plot(1, 2)
+    basic.pause(1000)
+    led.unplot(1, 2)
+    led.plot(1, 3)
+    basic.pause(1000)
+    led.unplot(1, 3)
+    led.plot(1, 4)
+    basic.pause(1000)
+    led.unplot(1, 4)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showArrow(ArrowNames.SouthWest)
+    led.plot(0, 0)
+    led.plot(0, 1)
+    led.plot(0, 2)
+    led.plot(0, 3)
+    led.plot(0, 4)
 })
 basic.showNumber(69)
 basic.showString("hh")
